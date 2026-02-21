806d799940c1b70cd5f89be58faa0a6518ff75b0
MD-ODOO <104442926+MD-ODOO@users.noreply.github.com>
2026-02-21 16:39:50 +0000

    Update executive_dashboard.js
    correction JS

--- oui_allo_rdv_pro/static/src/js/executive_dashboard.js
+++ oui_allo_rdv_pro/static/src/js/executive_dashboard.js
@@ -1,73 +1,65 @@
-/** oui_allo_rdv_pro/static/src/js/executive_dashboard.js **/
+/** @odoo-module **/
 
-odoo.define('oui_allo_rdv_pro.executive_dashboard', function(require) {
-    "use strict";
+import { registry } from "@web/core/registry";
+import { Component, onWillStart, useState } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
 
-    const { Component, useState, onMounted } = owl;
-    const rpc = require('web.rpc');
-    const { _t } = require('web.core');
+class ExecutiveDashboard extends Component {
+    setup() {
+        this.orm = useService("orm");
+        this.action = useService("action");
+        this.state = useState({
+            data: {
+                global: {},
+                conseillers: [],
+                campagnes: [],
+                is_admin: false,
+            },
+            period: "month",
+        });
 
-    class ExecutiveDashboard extends Component {
-        constructor() {
-            super(...arguments);
-            this.state = useState({
-                data: {
-                    global: {},
-                    conseillers: [],
-                    campagnes: [],
-                    is_admin: false,
-                },
-                period: 'month',
-            });
-        }
-
-        async fetchData() {
-            try {
-                const data = await rpc.query({
-                    model: 'oui.campaign',
-                    method: 'get_executive_dashboard_data',
-                    args: [this.state.period],
-                });
-
-                this.state.data.global = data.global ?? {};
-                this.state.data.conseillers = data.conseillers ?? [];
-                this.state.data.campagnes = data.campagnes ?? [];
-                this.state.data.is_admin = data.is_admin ?? false;
-
-            } catch (err) {
-                console.error("Erreur lors du chargement des données du dashboard :", err);
-            }
-        }
-
-        changePeriod(period) {
-            this.state.period = period;
-            this.fetchData();
-        }
-
-        openConseiller(conseillerId) {
-            return this.env.services.action.doAction({
-                type: 'ir.actions.act_window',
-                res_model: 'res.users',
-                res_id: conseillerId,
-                views: [[false, 'form']],
-            });
-        }
-
-        openCampaign(campId) {
-            return this.env.services.action.doAction({
-                type: 'ir.actions.act_window',
-                res_model: 'oui.campaign',
-                res_id: campId,
-                views: [[false, 'form']],
-            });
-        }
-
-        onMounted() {
-            this.fetchData();
-        }
+        onWillStart(async () => {
+            await this.fetchData();
+        });
     }
 
-    ExecutiveDashboard.template = 'ExecutiveDashboard';
+    async fetchData() {
+        const data = await this.orm.call(
+            "oui.campaign",
+            "get_executive_dashboard_data",
+            [this.state.period]
+        );
 
-    return ExecutiveDashboard;
-});
+        this.state.data.global = data.global || {};
+        this.state.data.conseillers = data.conseillers || [];
+        this.state.data.campagnes = data.campagnes || [];
+        this.state.data.is_admin = data.is_admin || false;
+    }
+
+    async changePeriod(period) {
+        this.state.period = period;
+        await this.fetchData();
+    }
+
+    openConseiller(conseillerId) {
+        return this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: "res.users",
+            res_id: conseillerId,
+            views: [[false, "form"]],
+        });
+    }
+
+    openCampaign(campId) {
+        return this.action.doAction({
+            type: "ir.actions.act_window",
+            res_model: "oui.campaign",
+            res_id: campId,
+            views: [[false, "form"]],
+        });
+    }
+}
+
+ExecutiveDashboard.template = "ExecutiveDashboard";
+
+registry.category("actions").add("executive_dashboard", ExecutiveDashboard);
